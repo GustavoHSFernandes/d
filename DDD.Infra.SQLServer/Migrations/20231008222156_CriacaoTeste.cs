9eb29cf9f1aff8a39a85835fe0304b5d80c5120d
miguelkawan2023 <miguelkawan2023@outlook.com>
2023-10-08 21:40:14 -0300

    commit

---
+++ DDD.Infra.SQLServer/Migrations/20231008222156_CriacaoTeste.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DDD.Infra.SQLServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTeste : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bibliotecarias",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false, defaultValueSql: "NEXT VALUE FOR [UserSequence]"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Sobrenome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bibliotecarias", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livros",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Autor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Paginas = table.Column<int>(type: "int", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livros", x => x.LivroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emprestimos",
+                columns: table => new
+                {
+                    EmprestimoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LivroId = table.Column<int>(type: "int", nullable: false),
+                    AlunoId = table.Column<int>(type: "int", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emprestimos", x => x.EmprestimoId);
+                    table.ForeignKey(
+                        name: "FK_Emprestimos_Livros_LivroId",
+                        column: x => x.LivroId,
+                        principalTable: "Livros",
+                        principalColumn: "LivroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emprestimos_LivroId",
+                table: "Emprestimos",
+                column: "LivroId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bibliotecarias");
+
+            migrationBuilder.DropTable(
+                name: "Emprestimos");
+
+            migrationBuilder.DropTable(
+                name: "Livros");
+        }
+    }
+}
---
+++ DDD.Infra.SQLServer/Repositories/EmprestimoRepositorySqlServer.cs
@@ -0,0 +1,68 @@
+﻿using DDD.Domain.BibliotecaContext;
+using DDD.Domain.SecretariaContext;
+using DDD.Infra.SQLServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Infra.SQLServer.Repositories
+{
+    public class EmprestimoRepositorySqlServer : IEmprestimoRepository
+    {
+        private readonly SqlContext _context;
+
+        public EmprestimoRepositorySqlServer(SqlContext context)
+        {
+            _context = context;
+        }
+        public void DeleteEmprestimo(Emprestimo emprestimo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Emprestimo GetEmprestimoById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Matricula> GetEmprestimos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Emprestimo InsertEmprestimo(int idLivro, int idAluno)
+        {
+            
+            var livro = _context.Livros.First(i => i.LivroId == idLivro);
+            var aluno = _context.Alunos.First(i => i.UserId == idAluno);
+
+            var emprestimo = new Emprestimo
+            {
+                Livro = livro
+                
+            };
+
+            try
+            {
+
+                _context.Add(emprestimo);
+                _context.SaveChanges();
+
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.InnerException;
+                throw;
+            }
+
+            return emprestimo;
+        }
+
+        public void UpdateEmprestimo(Emprestimo emprestimo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
